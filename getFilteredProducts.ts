14d8f668858459055225a7983cf162e42babe076
AbhishekSingh1909 <103984974+AbhishekSingh1909@users.noreply.github.com>
2023-10-18 02:08:03 +0300

    Delete getFilteredProducts.ts

--- getFilteredProducts.ts
+++
@@ -1,9 +0,0 @@
-import Product from "../types/Product";
-
-const getFilteredProducts = (products: Product[], search?: string) => {
-  return products.filter((p) =>
-    p.title.toLowerCase().includes(search?.toLowerCase() || "")
-  );
-};
-
-export default getFilteredProducts;
